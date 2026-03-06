0deecf4c6dace93312317684bee6b7e1bfed60e7
Adil Naeem <adilnaeem24a@gmail.com>
2026-03-06 22:22:46 +0500

    a

--- 46-51 react-router/src/routes/App.jsx
+++ 46-51 react-router/src/routes/App.jsx
@@ -23,7 +23,7 @@ function App() {
           <Header></Header>
           <Outlet/>
           <Footer></Footer>
-          <h1>adadadadssssssssssssssd</h1>
+          <h1>adadadxadssssssssssssssd</h1>
         </div>
       </div>
     </PostListProvider>
